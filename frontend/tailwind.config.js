13b79dba59a852ee3f3ef0aae4d72dbbbd20c567
Specticall <josephyusmita@gmail.com>
2024-03-03 23:03:33 +0700

    feat: created styles & icon component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        body: "#F9F1E9",
+        main: "#392A2A",
+        accent: "#FFD5A5",
+        danger: "#CC2A06",
+      },
+      fontSize: {
+        hero: "7.5rem",
+        body: ".875rem",
+        heading: "1rem",
+        title: "1.5rem",
+      },
+    },
   },
   plugins: [],
 };
