28d4a223648e9ca5f1f02fa8b08511ff639e2169
unknown <gazmanzara@gmail.com>
2024-03-08 18:04:54 +0700

    crud cart

--- backend/app/Models/Cart.php
+++ backend/app/Models/Cart.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+        'user_id'
+    ];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
